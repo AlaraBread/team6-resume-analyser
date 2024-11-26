bef26a718bf9cb9f0831ebddcee0aaff91562e25
julia <59781761+AlaraBread@users.noreply.github.com>
2024-11-26 18:20:14 -0500

    format

--- backend/api/upload/resume.ts
+++ backend/api/upload/resume.ts
@@ -25,8 +25,7 @@ export async function resumeUpload(ctx: Context) {
 	}
 
 	const formFile = formData.get("file");
-	const file =
-		typeof formFile == "string" || !formFile ? undefined : formFile;
+	const file = typeof formFile == "string" || !formFile ? undefined : formFile;
 
 	if (!file) {
 		ctx.response.status = 400;
