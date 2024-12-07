5f6baaac2f6fbc52aac37af9c3532f24b5b653a8
dylandunsheath <dtd35@njit.edu>
2024-12-06 22:27:29 -0500

    Update: Job Description Test - Exceeds Max Length

--- frontend/e2e/tests/form.spec.ts
+++ frontend/e2e/tests/form.spec.ts
@@ -22,4 +22,25 @@ test.describe("Form Page Tests", () => {
 
 		await expect(errorMessage).toBeVisible();
 	});
+
+	test("Show error when job description exceeds max length", async ({
+		page,
+	}) => {
+		const jobDescriptionInput = page.locator(
+			'textarea[placeholder="Enter job description"]',
+		);
+		const submitButton = page.locator(
+			'button:has-text("Submit Job Description")',
+		);
+		const errorMessage = page.locator(
+			"text=Job description must be under 5000 characters.",
+		);
+		// Generates a job description with 5001 characters
+		const longDescription = "A".repeat(5001);
+
+		// Fill with long string
+		await jobDescriptionInput.fill(longDescription);
+		await submitButton.click();
+		await expect(errorMessage).toBeVisible();
+	});
 });
