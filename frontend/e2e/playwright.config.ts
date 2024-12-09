27b4ddf4d7e0c57a3b2ba4658256777adda187aa
dylandunsheath <dtd35@njit.edu>
2024-12-09 17:01:27 -0500

    Update: playwright config file

--- frontend/e2e/playwright.config.ts
+++ frontend/e2e/playwright.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-	testDir: "./e2e/tests", // Test directory
-	timeout: 30000, // Timeout for each test
+	testDir: "./tests", // Test directory
+	timeout: 10000, // Timeout for each test
 	use: {
 		baseURL: "http://localhost:3000", // Base URL for app
 		headless: true, // Run in headless mode
