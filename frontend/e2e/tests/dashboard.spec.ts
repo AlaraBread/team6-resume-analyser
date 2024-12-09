60aebe3e5f9b2e503dc7d374dfa7a2edb43fca51
dylandunsheath <dtd35@njit.edu>
2024-12-09 17:48:59 -0500

    Update: dashboard.spec.ts test.describe change

--- frontend/e2e/tests/dashboard.spec.ts
+++ frontend/e2e/tests/dashboard.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("Form Page Tests", () => {
+test.describe("Dashboard Page Tests", () => {
 	test.beforeEach(async ({ page }) => {
 		// Add tests here
 	});
