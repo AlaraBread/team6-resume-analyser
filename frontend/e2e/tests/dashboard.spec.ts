cd0fa52b6fb2973ba9fd0bf00466506860481234
dylandunsheath <dtd35@njit.edu>
2024-12-09 11:28:40 -0500

    Update: Set-up dashboard.spec.ts

--- frontend/e2e/tests/dashboard.spec.ts
+++ frontend/e2e/tests/dashboard.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("Form Page Tests", () => {
-	test.beforeEach(async ({ page }) => {});
+	test.beforeEach(async ({ page }) => {
+		// Add tests here
+	});
 });
